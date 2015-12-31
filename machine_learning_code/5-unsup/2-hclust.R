373d12a1ab2b376f39ede2485c9f050908ec89a2
szilard <szilard@users.noreply.github.com>
2015-12-31 02:08:28 -0800

    shuffle iris for hclust

--- machine_learning_code/5-unsup/2-hclust.R
+++ machine_learning_code/5-unsup/2-hclust.R
@@ -1,5 +1,8 @@
 
-d <- iris[,-5]
+set.seed(123)
+iris_shuff <- iris[sample(nrow(iris)),]
+
+d <- iris_shuff[,-5]
 
 dd <- dist(d)
 hc <- hclust(dd, method = "average")
@@ -31,14 +34,16 @@ c0 <- iris[,5]
 
 dend %>% set("labels", NA) %>% 
   set("leaves_pch", 1) %>% set("leaves_cex", 0.4) %>%  
-  set("leaves_col", as.numeric(c0)) %>% hang.dendrogram(0) %>% plot
+  set("leaves_col", as.numeric(c0[as.numeric(labels(dend))])) %>% 
+  hang.dendrogram(0) %>% plot
 
 
 for (m in c("average","complete","single")) {
 d %>% dist %>% hclust(method = m) %>% as.dendrogram %>% 
   set("labels", NA) %>% 
   set("leaves_pch", 1) %>% set("leaves_cex", 0.4) %>%  
-  set("leaves_col", as.numeric(c0)) %>% hang.dendrogram(0) %>% plot(main = m)
+  set("leaves_col", as.numeric(c0[as.numeric(labels(dend))])) %>% 
+  hang.dendrogram(0) %>% plot(main = m)
 }  
   
 # more analysis
